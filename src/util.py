12e9c8f7177f828355a067e0dc2a7c9677a9cce1
Gus Beane <angus.beane@cfa.harvard.edu>
2020-04-28 15:03:30 -0400

    Add function for generating basepath from sim name

---
+++ src/util.py
@@ -0,0 +1,41 @@
+import numpy as np
+
+def basepath_from_sim(sim):
+    assert isinstance(sim, str), "sim must be a string"
+
+    base = '/n/hernquistfs3/IllustrisTNG/Runs/'
+
+    sim_paths = {'TNG300-3': base + 'L205n625TNG/',
+                 'TNG300-3-Dark': base + 'L205n625TNG_DM/',
+                 'TNG300-2': base + 'L205n1250TNG/',
+                 'TNG300-2-Dark': base + 'L205n1250TNG_DM/',
+                 'TNG300-1': base + 'L205n2500TNG/',
+                 'TNG300-1-Dark': base + 'L205n2500TNG_DM/',
+   
+                 'TNG100-3': base + 'L75n455TNG/',
+                 'TNG100-3-Dark': base + 'L75n455TNG_DM/',
+                 'TNG100-2': base + 'L75n910TNG/',
+                 'TNG100-2-Dark': base + 'L75n910TNG_DM/',
+                 'TNG100-1': base + 'L75n1820TNG/',
+                 'TNG100-1-Dark': base + 'L75n1820TNG_DM/',
+    
+                 'TNG50-4': base + 'L35n270TNG/',
+                 'TNG50-4-Dark': base + 'L35n270TNG_DM/',
+                 'TNG50-3': base + 'L35n540TNG/',
+                 'TNG50-3-Dark': base + 'L35n540TNG_DM/',
+                 'TNG50-2': base + 'L35n1080TNG/',
+                 'TNG50-2-Dark': base + 'L35n1080TNG_DM/',
+                 'TNG50-1': base + 'L35n2160TNG/',
+                 'TNG50-1-Dark': base + 'L35n2160TNG_DM/'}
+
+    if sim in sim_paths.keys():
+        return sim_paths[sim]
+    else:
+        raise NotImplementedError('I don\'t recognize sim: '+sim)
+
+if __name__ == '__main__':
+    assert basepath_from_sim('TNG300-1') == '/n/hernquistfs3/IllustrisTNG/Runs/L205n2500TNG/'
+    assert basepath_from_sim('TNG100-1') == '/n/hernquistfs3/IllustrisTNG/Runs/L75n1820TNG/'
+    assert basepath_from_sim('TNG50-1') == '/n/hernquistfs3/IllustrisTNG/Runs/L35n2160TNG/'
+    assert basepath_from_sim('TNG300-2-Dark') == '/n/hernquistfs3/IllustrisTNG/Runs/L205n1250TNG_DM/'
+
