8adc915d6cdd6df92dcddf9f58e8241e5a8c6845
Gus Beane <angus.beane@cfa.harvard.edu>
2020-04-30 18:48:22 -0400

    Create gen_ids_of_interest function in src/gen.py

--- src/gen.py
+++ src/gen.py
@@ -1,5 +1,6 @@
 import numpy as np
 from os import path
+import h5py as h5
 
 def gen_all_snaps(basepath, subbox=None):
     assert isinstance(basepath, str)
@@ -31,6 +32,64 @@ def gen_all_snaps(basepath, subbox=None):
 
     return np.array(snap_list)
 
+def gen_ids_of_interest(filepath, position, radius, mode='radius_position', PartType=None):
+    assert isinstance(filepath, str)
+    assert path.exists(filepath)
+    assert isinstance(radius, float)
+    assert isinstance(position, (np.ndarray, list))
+
+    if isinstance(position, list):
+        position = np.array(position)
+    assert len(position)==3
+
+    if mode == 'blackhole':
+        return _gen_ids_blackhole(filepath, position, radius)
+
+    assert isinstance(PartType, (int, list, np.ndarray))
+    if isinstance(PartType, int):
+        PartType = [PartType]
+
+    if mode == 'radius_position':
+        return _gen_ids_radius_position(filepath, position, radius, PartType)
+
+    raise NotImplementedError("mode="+mode+" is not implemented")
+
+def _gen_ids_radius_position(filepath, position, radius, PartType):
+    dat = h5.File(filepath, mode='r')
+    ids_list = np.array([], dtype=np.uint64)
+
+    for pt in PartType:
+        ptype = 'PartType' + str(pt)
+        assert ptype in dat.keys(), "I don\'t recognize particle type: "+str(pt)
+        
+        pos = np.array(dat[ptype]['Coordinates'])
+        pos = np.subtract(pos, position)
+
+        keys = np.where(np.linalg.norm(pos, axis=1) < radius)[0]
+        ids_list = np.concatenate((ids_list, dat[ptype]['ParticleIDs'][keys]))
+
+    return ids_list
+
+def _gen_ids_blackhole(filepath, position, radius):
+    dat = h5.File(filepath, mode='r')
+    
+    ptype = 'PartType5'
+    if ptype not in dat.keys():
+        return None
+
+    pos = np.array(dat[ptype]['Coordinates'])
+    pos = np.subtract(pos, position)
+
+    keys = np.where(np.linalg.norm(pos, axis=1) < radius)[0]
+    if len(keys) > 0:
+        masses = dat[ptype]['Masses'][keys]
+        subkey = np.argmax(masses)
+        print(pos[keys[subkey]], masses[subkey])
+        return (dat[ptype]['ParticleIDs'][keys[subkey]], masses[subkey])
+    else:
+        return None
+
+
 if __name__ == '__main__':
     from util import basepath_from_sim
 
@@ -41,3 +100,13 @@ if __name__ == '__main__':
     snap_list = gen_all_snaps(basepath, subbox=1)
     print(snap_list) 
 
+    position = [37448.52, 42239.434, 66705.08]
+    radius = 230.64125
+
+    for i in range(7):
+        fpath = '../data/snap_subbox1_2430.'+str(i)+'.hdf5'
+        print(gen_ids_of_interest(fpath, position, radius, 'blackhole'))
+    
+    for i in range(7):
+        fpath = '../data/snap_subbox1_2430.'+str(i)+'.hdf5'
+        print(gen_ids_of_interest(fpath, position, radius, 'radius_position', 0))
