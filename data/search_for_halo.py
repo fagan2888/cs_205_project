08deba2472ba7211d6d734faeead704ab6cf80b1
Gus Beane <angus.beane@cfa.harvard.edu>
2020-05-06 09:46:47 -0400

    Update to add lvl1 and lvl2 MW data

--- data/search_for_halo.py
+++ data/search_for_halo.py
@@ -1,5 +1,15 @@
 import numpy as np 
 import h5py as h5 
+import sys
+import glob
+
+if len(sys.argv)>1:
+    name = 'TNG100-'+sys.argv[1]
+    assert name in ['TNG100-3', 'TNG100-2', 'TNG100-1']
+else:
+    name = 'TNG100-1'
+
+print(name)
 
 center = np.array([37000, 43500, 67500])
 boxsize = 7500
@@ -9,25 +19,30 @@ hlittle=0.6774
 print('center=', center/1000)
 print('boxsize=', boxsize/1000)
 
-for i in range(7):
-	fname = 'fof_subhalo_tab_099.' + str(i) + '.hdf5'
-	fof = h5.File(fname, mode='r')
+fof_list = glob.glob(name + '/fof_subhalo_tab_099.*.hdf5')
 
-	group_pos = np.array(fof['Group']['GroupPos'])
-	group_pos = np.subtract(group_pos, center)
+for fname in fof_list:
+    fof = h5.File(fname, mode='r')
 
-	xbool = np.abs(group_pos[:,0]) < boxsize/2.0
-	ybool = np.abs(group_pos[:,1]) < boxsize/2.0
-	zbool = np.abs(group_pos[:,2]) < boxsize/2.0
+    if 'GroupPos' not in fof['Group'].keys():
+        continue
 
-	keys = np.where(np.logical_and(np.logical_and(xbool, ybool), zbool))[0]
+    group_pos = np.array(fof['Group']['GroupPos'])
+    group_pos = np.subtract(group_pos, center)
 
-	xdist = boxsize/2.0 - np.abs(group_pos[:,0])
-	ydist = boxsize/2.0 - np.abs(group_pos[:,1])
-	zdist = boxsize/2.0 - np.abs(group_pos[:,2])
+    xbool = np.abs(group_pos[:,0]) < boxsize/2.0
+    ybool = np.abs(group_pos[:,1]) < boxsize/2.0
+    zbool = np.abs(group_pos[:,2]) < boxsize/2.0
 
-	dist_to_box = np.minimum(np.minimum(xdist, ydist), zdist)
-	dist_to_box /= boxsize/2.0
+    keys = np.where(np.logical_and(np.logical_and(xbool, ybool), zbool))[0]
 
-	for k in keys:
-		print(fof['Group']['GroupPos'][k], dist_to_box[k], fof['Group']['Group_M_Mean200'][k]/hlittle, fof['Group']['Group_R_Mean200'][k])
+    xdist = boxsize/2.0 - np.abs(group_pos[:,0])
+    ydist = boxsize/2.0 - np.abs(group_pos[:,1])
+    zdist = boxsize/2.0 - np.abs(group_pos[:,2])
+
+    dist_to_box = np.minimum(np.minimum(xdist, ydist), zdist)
+    dist_to_box /= boxsize/2.0
+
+    for k in keys:
+        if fof['Group']['Group_M_Mean200'][k]/hlittle > 80:
+            print(fof['Group']['GroupFirstSub'][k], fof['Group']['GroupPos'][k], dist_to_box[k], fof['Group']['Group_M_Mean200'][k]/hlittle, fof['Group']['Group_R_Mean200'][k])
