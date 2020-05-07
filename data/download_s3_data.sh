6ce7fb2bf706843044c8b89945c20bf44817af6e
Nam Luu <namluunhat164@gmail.com>
2020-05-07 12:05:23 -0400

    modify generate_data.py

---
+++ data/download_s3_data.sh
@@ -0,0 +1,9 @@
+#~/bin/bash
+
+mkdir -p s3
+cd s3
+wget -O tracers_info https://spark-illustris-tng.s3.amazonaws.com/tracers_info
+echo 'Finish download tracers info'
+wget -O particles_info https://spark-illustris-tng.s3.amazonaws.com/particles_info
+echo 'Finish download particles info'
+wget -O https://spark-illustris-tng.s3.amazonaws.com/tracers_info_full
--- src/generate_bin.py
+++ src/generate_bin.py
@@ -3,27 +3,28 @@ from util import *
 from gen import gen_position_of_subhalo
 import numpy as np
 
-tracers_info = read_dataset('spark-illustris-tng/tracers_info')
-particles_info = read_dataset('spark-illustris-tng/particles_info')
+tracers_info = read_dataset('../data/s3/tracers_info_full')
+particles_info = read_dataset('../data/s3/particles_info')
 print('finish loading data')
 
-snap = 1604
-subhalo_pos_list = gen_position_of_subhalo(19391, '../data/TNG100-3/subbox1_99.hdf5')
-subhalo_pos = subhalo_pos_list[snap]
+for snap in range(0,2430):
+    print('snap: ' + str(snap))
+    subhalo_pos_list = gen_position_of_subhalo(19391, '../data/TNG100-3/subbox1_99.hdf5')
+    subhalo_pos = subhalo_pos_list[snap]
 
-spin = np.array([396.40796,-595.03217,-1046.6497 ])
+    spin = np.array([396.40796,-595.03217,-1046.6497 ])
 
-tracers = tracers_info[tracers_info[:,0] == snap]
-particles = particles_info[particles_info[:,0] == snap]
+    tracers = tracers_info[tracers_info[:,0] == snap]
+    particles = particles_info[particles_info[:,0] == snap]
 
-gas_pos = particles[:,1:4]
-gas_mass = particles[:,4]
+    gas_pos = particles[:,1:4]
+    gas_mass = particles[:,4]
 
-tracer_pos = tracers[:,1:4]
-tracer_mass = tracers[:,4]
-tracer_snap = tracers[:,5]
+    tracer_pos = tracers[:,1:4]
+    tracer_mass = tracers[:,4]
+    tracer_snap = tracers[:,5].astype(int)
 
-gas_map, tracer_map = output_binned_map(1604, spin, subhalo_pos, gas_pos, gas_mass, tracer_pos, tracer_mass, tracer_snap)
+    gas_map, tracer_map = output_binned_map(1604, spin, subhalo_pos, gas_pos, gas_mass, tracer_pos, tracer_mass, tracer_snap)
 
-print(np.sum(gas_map), np.sum(tracer_map))
+    print(np.sum(gas_map), np.sum(tracer_map))
 
