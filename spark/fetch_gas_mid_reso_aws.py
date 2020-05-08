d496e9685604799e5bb2cf5d316c9443ed61bc25
Nam Luu <namluunhat164@gmail.com>
2020-05-08 00:23:51 -0400

    add script to get data for mid resolution

---
+++ spark/fetch_gas_mid_reso_aws.py
@@ -0,0 +1,72 @@
+from pyspark import SparkConf, SparkContext
+import h5py
+from datetime import datetime
+import numpy as np
+import s3fs
+import os
+
+### FUNCTIONS
+
+def get_snapshot_number(fpath):
+    return int(fpath.split('/')[-2][len('snapshot-'):])
+
+def extract_gas_info(filepath, position, radius, snapnum):
+    dat = h5py.File(s3.open(filepath, 'rb'), mode='r')
+
+    ptype = 'PartType0'
+    if (ptype not in dat.keys()):
+        return []
+
+    pos = np.array(dat[ptype]['Coordinates'])
+    pos = np.subtract(pos, position)
+    keys = np.where(np.linalg.norm(pos, axis=1) < radius)[0]
+
+    coordinates = dat[ptype]['Coordinates'][keys]
+    masses = dat[ptype]['Masses'][keys].reshape(-1,1)
+    volume = dat[ptype]['Masses'][keys] / dat[ptype]['Density'][keys]
+    size = np.power(volume, 1.0/3).reshape(-1,1)
+
+    return np.hstack((np.full((masses.shape[0], 1), snapnum), coordinates, masses, size))
+    
+def extract_gas_from_snap(snap, position, radius):
+    flist = snap_tofile(snap)
+    files = s3.glob(flist)
+    
+    ret = np.array([]).reshape(0, 6)
+    for f in files:
+        ret = np.vstack((ret, extract_gas_info(f, position, radius, snap)))
+    
+    return ret
+    
+def extract_map(snap):
+    position = subhalo_positions_bc.value[snap]
+    ret = extract_gas_from_snap(snap, position, radius)
+    return ret
+    
+def snap_tofile(snap):
+    return 'spark-illustris-tng/tng100-2/subbox1/snapdir_subbox1_{0}/*.hdf5'.format(snap)
+    
+### VARIABLES
+
+ACCESS_KEY = os.environ['SPARK_ACCESS_KEY']
+SECRET_KEY = os.environ['SPARK_SECRET_KEY']
+
+timestamp = datetime.now().strftime("%m%d_%H%M")
+print("timestamp: " + str(timestamp))
+
+s3 = s3fs.S3FileSystem(key=ACCESS_KEY, secret=SECRET_KEY)
+
+subhalo_id = 89587
+subhalo_positions = get_subhalo_pos(s3)
+radius = 140
+
+### SPARK
+
+sc = SparkContext(conf = conf)
+subhalo_positions_bc = sc.broadcast(subhalo_positions)
+
+df = sc.parallelize(range(2000, 4380)) \
+  .repartition(10)
+  .flatMap(extract_map)
+
+df.saveAsTextFile('s3a://spark-namluu-output/illustrisdata_{0}'.format(timestamp))
